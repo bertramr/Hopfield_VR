74d851f362cfa996649b02636d5ff18b067f7ebf
Reinhold Bertram <reinhold.bertram@rwth-aachen.de>
2012-04-22 20:45:51 +0200

    Excersice 1

--- src/HopfieldNetwork.py
+++ src/HopfieldNetwork.py
@@ -1,4 +1,7 @@
 from pylab import *
+from copy import copy
+from time import sleep
+tmax = 20
 
 class HopfieldNetwork:
     def __init__(self,N):
@@ -12,28 +15,36 @@ class HopfieldNetwork:
 
         """
 
-    def create_pattern(self,P=1,ratio=0.5)
+    def create_pattern(self,P=1,ratio=0.5):
         self.P = P
-        self.pattern = -ones((P,self.N,int)
+        self.pattern = -ones((P,self.N),int)
+
         idx = int(ratio*self.N)
         for i in range(P):
             self.pattern[i,:idx] = 1
             self.pattern[i] = permutation(self.pattern[i])
 
     def calc_weight(self):
-        self.weight = zeros(self.N,self.N)
+        self.weight = zeros((self.N,self.N))
         
         for i in range(self.N):
             for j in range(self.N):
                 self.weight[i,j] = 1./self.N * sum(self.pattern[mu, i] * self.pattern[mu,j] for mu in range(self.P))
         
-    def set_init_state(self,mu=0,flip_ratio=0):
+    def set_init_state(self,mu,flip_ratio):
         self.x = copy(self.pattern[mu])
         flip = permutation(arange(self.N))
         idx = int(self.N*flip_ratio)
         self.x[flip[0:idx]] *= -1
-        t = [0]
+        self.t = [0]
         overlap = [self.overlap(mu)]
+
+    def energy(self):
+        h = 0        
+        for i in range(self.N):
+            for j in range(self.N):
+                h = h + self.weight[i,j] * self.x[i] * self.x[j]
+        return -h
 
     def dynamic(self):
         """
@@ -45,8 +56,9 @@ class HopfieldNetwork:
 
         h = sum(self.weight*self.x,axis=1)
         self.x = sign(h)
+        
 
-    def overlap(self,mu):
+    def overlap(self,mu=0):
         """
         DEFINITION
         computes the overlap of the test pattern with pattern nb mu
@@ -59,7 +71,7 @@ class HopfieldNetwork:
 
         return 1./self.N*sum(self.pattern[mu]*self.x)
 
-    def run(self):
+    def run(self,mu=0, flip_ratio=0.2):
         """
         DEFINITION
         runs the dynamics and plots it in an awesome way
@@ -73,16 +85,21 @@ class HopfieldNetwork:
         -N.Fremaux 03.2010.
         """
         
-        try:
-            self.pattern[mu]
-        except:
-            raise IndexError, 'pattern index too high'
+      #  try:
+     #       self.pattern[mu]
+     #   except:
+ #           raise IndexError, 'pattern index too high'
         
+        self.create_pattern()
+        self.calc_weight()
+        self.set_init_state(mu,flip_ratio)
 
-        
+        t = [0]
+        overlap = [self.overlap(mu)]
+        energy = [self.energy()]
         # prepare the figure
         figure()
-        
+        '''
         # plot the current network state
         subplot(221)
         g1 = imshow(self.grid(),**plot_dic)# we keep a handle to the image
@@ -94,7 +111,14 @@ class HopfieldNetwork:
         imshow(self.grid(mu=mu),**plot_dic)
         axis('off')
         title('pattern %i'%mu)
-        
+        '''       
+
+        subplot(211)
+        g1, = plot(t,energy,'k',lw=2)
+        axis([0,tmax,-self.N,self.N])
+        xlabel('time step')
+        ylabel('energy')
+
         # plot the time course of the overlap
         subplot(212)
         g2, = plot(t,overlap,'k',lw=2) # we keep a handle to the curve
@@ -103,9 +127,12 @@ class HopfieldNetwork:
         ylabel('overlap')
         
         # this forces pylab to update and show the fig.
+
+
         draw()
         
         x_old = copy(self.x)
+        
         
         for i in range(tmax):
 
@@ -113,9 +140,10 @@ class HopfieldNetwork:
             self.dynamic()
             t.append(i+1)
             overlap.append(self.overlap(mu))
+            energy.append(self.energy())
             
             # update the plotted data
-            g1.set_data(self.grid())
+            g1.set_data(t, energy)
             g2.set_data(t,overlap)
             
             # update the figure so that we see the changes
