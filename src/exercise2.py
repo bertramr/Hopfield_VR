ec529d3db89016989d29c20888ddadccace3e2d4
Reinhold Bertram <reinhold.bertram@rwth-aachen.de>
2012-05-14 00:34:43 +0200

    exercise3 mit plot, 2 und 3 mit pickle

--- src/exercise2.py
+++ src/exercise2.py
@@ -1,18 +1,22 @@
 from Hopfield import HopfieldNetwork
 from pylab import *
 import numpy as np
+import pickle
 
 '''
         We now define the capacity of a Hopfield network of size N as the number of patterns PN,max that
         can be stored, such that the mean retrieval error averaged over all stored patterns (one retrieval
         attempt each) is at most 2%. This yields the maximal load N,max = PN,max . N
         
-        Set c=0.1. Calculate N,max for at least 10 network realizations and state the mean together with confidence intervals. Do this for N = 100, 250 and one other larger network size. 
+        Set c=0.1. Calculate N,max for at least 10 network realizations and state the mean together 
+        with confidence intervals. Do this for N = 100, 250 and one other larger network size. 
         
         Shortly interpret the resulting values and compare with results from literature.
         
         Hint: To calculate Pmax, successively add patterns and calculate the mean retrieval error over.
-    '''
+'''
+
+
 def pmax(N,flip_ratio,pcut):
     h = HopfieldNetwork()
     p=1
@@ -26,19 +30,28 @@ def pmax(N,flip_ratio,pcut):
         print '%d: %.4f' %( p , meanerror)
     return p-1
 
-def exercise2(N=200,tests=10,confidence=0.95):
+def exercise2(N=500,tests=10,confidence=0.95):
     print 'Exercise 2:'
-    pmaxval=zeros((1,tests),float)
-    for i in range(tests):
-        pmaxval[0,i]=pmax(N,flip_ratio=0.1,pcut=0)
-    loadmax=pmaxval[0,:]/N
-    load_mean=np.mean(loadmax)
-    pmax_mean=np.mean(pmaxval[0,:])
-    load_std=np.std(loadmax)
-    ci = load_std * ((1+confidence)/2)/ 10
+    pmaxval=zeros((3,tests),float)
+    n = 0
+    for N in (100,250,N):
+        print N
+        for i in range(tests):
+            pmaxval[n,i]=pmax(N,flip_ratio=0.1,pcut=0)
+        loadmax=pmaxval[n,:]/N
+        load_mean=np.mean(loadmax)
+        pmax_mean=np.mean(pmaxval[n,:])
+        load_std=np.std(loadmax)
+        ci = load_std * ((1+confidence)/2)/ 10
+        
+        n += 1
 
-    print 'The maximal load of the Hopfield Network is %.4f with a 0.95 confidence intervall of [%.4f,%.4f]. The capacity of the network is %.4f'%(load_mean,load_mean-ci,load_mean+ci,pmax_mean)        
+        print 'The maximal load of the Hopfield Network with N=%d is %.4f with a 0.95 confidence intervall of [%.4f,%.4f].\nThe capacity of the network is %.4f'%(N, load_mean,load_mean-ci,load_mean+ci,pmax_mean)        
     
+    with open('../tex/img/plots/exercise2_pmaxval','w') as f:
+        pickle.dump(pmaxval,f)
+    f.closed
+
 if __name__=="__main__":
     exercise2()
 
--- src/exercise3.py
+++ src/exercise3.py
@@ -2,8 +2,9 @@ from Hopfield import HopfieldNetwork
 from exercise2 import pmax
 import numpy as np
 from pylab import *
+import pickle
 
-def exercise3(N = 200, c= 0.1, pcut=0, repetition=10):
+def exercise3(N = 200, c= 0.1, confidence=0.95, numint = 11, repetition=10):
     '''
     Exercise 3: Random asymmetry
     To investigate the robustness of pattern retrieval against asymmetry in the
@@ -17,10 +18,35 @@ def exercise3(N = 200, c= 0.1, pcut=0, repetition=10):
     Note: Bare in mind that the convergence assertion of Question 1.1 does not 
     necessarily hold for pcut > 0.
     '''
-    
+    print 'Exercise 3:'
+    pmaxval= zeros((numint,repetition),float)
     for rep in range(repetition):
-        for pcut in np.linspace(0,1,num=11):
-            pmax(N=N,flip_ratio=c,pcut=pcut)
+         i = 0
+         for pcut in np.linspace(0,1,num=numint):
+            pmaxval[i,rep] = pmax(N=N,flip_ratio=c,pcut=pcut)
+            i += 1
+    loadmax = pmaxval[:,:]/N
+    load_mean = np.mean(loadmax,axis=1)
+    pmax_mean = np.mean(pmaxval[:,:],axis=1)
+    load_std = np.std(loadmax,axis=1)
+    ci = load_std * ((1+confidence)/2)/ 10
+
+    fig = figure()
+    ax1 = fig.add_subplot(111)
+    lp = errorbar(np.linspace(0,1,num=numint),load_mean,load_std)
+    
+    ax1.set_ylim(0,1)
+    ax1.yaxis.grid(True,linestyle='-',which='major',color=(0.2,0.2,0.2),alpha=0.5)
+    ax1.set_axisbelow(True)
+    ax1.set_title('Mean maximal load over different P_cut (Q=%d)' % (repetition))
+    ax1.set_ylabel('mean maximal load')
+    ax1.set_xlabel('P_cut')
+    savefig('../tex/img/plots/mean_max_load-%d.png' % repetition)
+    close()
+    
+    with open('../tex/img/plots/exercise3_pmaxval','w') as f:
+        pickle.dump(pmaxval,f)
+    f.closed
 
 if __name__=="__main__":
     exercise3()
